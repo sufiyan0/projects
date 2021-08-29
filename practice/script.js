9c095a7a78bac84b69e4b818ca78083e66467e6d
sufiyan0 <86568496+sufiyan0@users.noreply.github.com>
2021-08-29 16:56:31 +0500

    project 6 practice
    Project 6 landing page practice completed

---
+++ practice/script.js
@@ -0,0 +1,13 @@
+// const tuggleButton=document.getElementById('toggle');
+
+// tuggleButton.addEventListener('click',() =>{
+//     document.body.classList.toggle('show-toggle')
+// } )
+
+
+const menuToggle = document.getElementById('toggle')
+
+
+menuToggle.addEventListener('click', () => {
+    document.body.classList.toggle('show-nav')
+} )
