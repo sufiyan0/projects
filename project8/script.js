10ec4b486a2e3675c3a76cc7a7236171b0d8dd19
sufiyan0 <86568496+sufiyan0@users.noreply.github.com>
2021-09-18 21:36:57 +0500

    Project 8
    project 8 completed

--- project8/script.js
+++ project8/script.js
@@ -43,9 +43,82 @@ function searchMeal(e) {
     } else {
         alert('Please Enter what you want to search')
     };
+
+    selectedMeal.innerHTML = '';
+
+};
+
+// Function to get details of selected meal
+function getMeal(mealId) {
+    //fetch meal details through mealId
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+    .then(res => res.json())
+    .then(data => {
+        const meal = data.meals[0];
+        displayMealDetails(meal);
+    });
+}
+
+// Function to render meal display in UI
+function displayMealDetails(meal) {
+    meals.innerHTML = '';
+    resultHeading.innerHTML = '';
+    const ingredients = [];
+    // loop for ingredients attribute
+    for ( let i = 1; i<= 20; i++) {
+        if (meal[`strIngredient${i}`]){
+            // Push all the ingredients and measurement into the ingredients array
+            ingredients.push(`${meal[`strIngredient${i}`]} : ${meal[`strMeasure${i}`]} `);
+        } else { 
+            break;
+        }
+        
+    };
+
+    //rander data into UI
+    selectedMeal.innerHTML = `
+    <div class = "selected-meal-details">
+        <h1>${meal.strMeal}  </h1>
+        <img src = "${meal.strMealThumb}" alt = "${meal.strMeal}" />
+        <div class = "selected-meal-info" >
+            ${meal.strCategory ? `<p> ${meal.strCategory}</p>` : ''}
+            ${meal.strArea ? `<p> ${meal.strArea}</p>` : ''}
+        </div>
+
+        <h2> Ingredients </h2>
+        <ul>
+            ${ingredients.map( ingredient => `<li> ${ingredient} </li>` ).join(' ') }
+        </ul>
+
+        <h2> Instructions for Cooking </h2>
+        <div class = "selected-meal-instruction">
+        <p> ${meal.strInstructions} </p>
+        </div>
+
+    </div>
+
+    `;
 };
 
 
 //Event Listeners
 // 1.Event Listeners for form submit
 submit.addEventListener('submit', searchMeal)
+
+// 2.listen to check on meals
+meals.addEventListener('click', e => {
+    const mealInfo = e.path.find(item => {
+        if (item.classList) {
+            return item.classList.contains('meal-info')
+        } else {
+            return false;
+        }
+    });
+
+    if (mealInfo) {
+        const mealId = mealInfo.getAttribute('data-mealid');
+        //fetch details of meal   
+        getMeal(mealId);
+     }
+
+}); 
